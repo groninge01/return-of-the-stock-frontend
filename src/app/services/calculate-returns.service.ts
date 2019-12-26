99159139ecb540d8b5264686ddf1cf6b02dd9527
groninge01 <groninge@hotmail.com>
2019-12-26 13:46:40 +0100

    Update variables

--- src/app/services/calculate-returns.service.ts
+++ src/app/services/calculate-returns.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { environment } from '../../environments/environment';
+
 import { ChartDataRequest } from 'src/app/data/data.model';
 
 @Injectable({
@@ -10,12 +12,9 @@ export class CalculateReturnsService {
   constructor(private http: HttpClient) {}
 
   post(request: ChartDataRequest) {
-    return this.http.post('apitest/', {
-      startingCapitalAmount: request.startingCapitalAmount,
-      additionAmount: request.additionAmount,
-      returnPercentage: request.returnPercentage,
-      numberOfPeriods: request.numberOfPeriods,
-      typeOfPeriod: request.typeOfPeriod
-    });
+    return this.http.post(
+      `${environment.apiHost}${environment.apiUrl}`,
+      request
+    );
   }
 }
