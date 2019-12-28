6fcac779c461e71101fcab3962dcbc5b2e4e1cf4
groninge01 <groninge@hotmail.com>
2019-12-28 10:46:20 +0100

    update app name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,9 +4,7 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent]
     }).compileComponents();
   }));
 
@@ -16,16 +14,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'testapp'`, () => {
+  it(`should have as title 'finance-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('testapp');
+    expect(app.title).toEqual('finance-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('testapp app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain(
+      'finance-app app is running!'
+    );
   });
 });
