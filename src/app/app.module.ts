39c6914aaf19b6dfb09a5694a8261a616655c2bc
groninge01 <groninge@hotmail.com>
2020-01-10 21:01:05 +0100

    group components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,11 +17,11 @@ import { MaterialModule } from './material/material.module';
 
 import { AppComponent } from './app.component';
 import { FormComponent } from './components/form/form.component';
+import { ChartOutputComponent } from './components/chart-output/chart-output.component';
 
 import { CalculateReturnsService } from 'src/app/services/calculate-returns.service';
 
 import { environment } from '../environments/environment';
-import { ChartOutputComponent } from './components/chart-output/chart-output.component';
 
 @NgModule({
   declarations: [AppComponent, FormComponent, ChartOutputComponent],
